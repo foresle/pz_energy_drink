ea085aa9a94bf1590f5e251d658545905620c684
foresle <foresle@noleron.com>
2023-12-11 02:52:42 +0200

    Added spawn script.

---
+++ media/lua/server/Items/EnergyDrinksByForesleDistribution.lua
@@ -0,0 +1,50 @@
+require 'Items/SuburbsDistributions'
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions.list["FridgeBottles"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(ProceduralDistributions.list["FridgeBottles"].items, 14);
+table.insert(ProceduralDistributions.list["FridgeBottles"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(ProceduralDistributions.list["FridgeBottles"].items, 14);
+
+table.insert(ProceduralDistributions.list["BarCounterMisc"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(ProceduralDistributions.list["BarCounterMisc"].items, 14);
+table.insert(ProceduralDistributions.list["BarCounterMisc"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(ProceduralDistributions.list["BarCounterMisc"].items, 14);
+
+table.insert(ProceduralDistributions.list["FridgeSoda"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(ProceduralDistributions.list["FridgeSoda"].items, 14);
+table.insert(ProceduralDistributions.list["FridgeSoda"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(ProceduralDistributions.list["FridgeSoda"].items, 14);
+
+table.insert(ProceduralDistributions.list["GigamartBottles"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(ProceduralDistributions.list["GigamartBottles"].items, 14);
+table.insert(ProceduralDistributions.list["GigamartBottles"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(ProceduralDistributions.list["GigamartBottles"].items, 14);
+
+table.insert(ProceduralDistributions.list["KitchenBottles"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(ProceduralDistributions.list["KitchenBottles"].items, 14);
+table.insert(ProceduralDistributions.list["KitchenBottles"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(ProceduralDistributions.list["KitchenBottles"].items, 14);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 14);
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 14);
+
+table.insert(ProceduralDistributions.list["StoreShelfDrinks"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(ProceduralDistributions.list["StoreShelfDrinks"].items, 14);
+table.insert(ProceduralDistributions.list["StoreShelfDrinks"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(ProceduralDistributions.list["StoreShelfDrinks"].items, 14);
+
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, 14);
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, 14);
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, "EnergyDrinks.AnEnergyDrink");
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, 14);
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, 14);
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, 14);
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, "EnergyDrinks.OPSPEnergyDrink");
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, 14);
